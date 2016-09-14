195ab5cba29dd5377c00ecc574a25508b84b973d
DickinsonMark <monoposon@comcast.net>
2016-09-14 15:20:58 -0600

    working n the add new restaurant route, needed to pull changes from master

--- src/server/config/route-config.js
+++ src/server/config/route-config.js
@@ -6,9 +6,11 @@
 
     // *** routes *** //
     const routes = require('../routes/index');
+    const oneRestRoutes = require('../routes/restaurant');
 
     // *** register routes *** //
     app.use('/', routes);
+    app.use('/restaurant', oneRestRoutes);
 
   };
 
---
+++ src/server/routes/restaurant.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'];
+const oneRestC = require('../controllers/oneRest');
+
+router.get('/:id/edit', (req, res, next) => {
+  const renderObj = {};
+  renderObj.title = 'Create A New Restaurant!';
+  renderObj.states =
+  res.render('new', renderObj);
+});
