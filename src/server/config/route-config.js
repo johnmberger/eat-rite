2740a5ab8da2ea636525194f8df705e63ba013a7
Allen Fordham <allen.s.fordham@gmail.com>
2016-09-14 11:08:12 -0600

    creating review page

--- src/server/config/route-config.js
+++ src/server/config/route-config.js
@@ -6,9 +6,11 @@
 
     // *** routes *** //
     const routes = require('../routes/index');
+    const review = require('../routes/review.html')
 
     // *** register routes *** //
     app.use('/', routes);
+    app.use('/review', review)
 
   };
 
