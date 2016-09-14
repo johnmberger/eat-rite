a23222214c4ea97c32a55069bf8a1cefef3251ac
johnmberger <johnmartinberger@gmail.com>
2016-09-14 16:32:20 -0600

    added restaurant photos and updated index route

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -4,11 +4,17 @@ const knex = require('../db/knex');
 
 router.get('/', (req, res, next) => {
 
-  knex('restaurants').select('*').join('addresses', 'addresses.id', 'restaurants.address_id')
+  knex('restaurants')
+  .select('*', 'restaurants.id as restaurant_id').join('addresses', 'addresses.id', 'restaurants.address_id')
   .then((restaurants) => {
     const randNum = Math.ceil(Math.random() * restaurants.length);
+
+    restaurants.forEach((restaurant) => {
+      restaurant.url = `./img/${restaurant.cuisine_type}.jpeg`
+    });
+
     res.render('index', {
-      title: 'You Betta\' Eat Rite!',
+      title: 'Eat Rite  | You Betta\' Eat Rite!',
       restaurants: restaurants.slice(randNum, randNum + 3)
     });
   });
