5bb71e0221fb942aca46f8258ad41b49bbd6df85
johnmberger <johnmartinberger@gmail.com>
2016-09-14 12:32:05 -0600

    styled index page a little bit

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -4,11 +4,12 @@ const knex = require('../db/knex');
 
 router.get('/', (req, res, next) => {
 
-  knex('restaurants').select('*')
+  knex('restaurants').select('*').join('addresses', 'addresses.id', 'restaurants.address_id')
   .then((restaurants) => {
+    const randNum = Math.ceil(Math.random() * restaurants.length);
     res.render('index', {
-      title: 'Welcome to our site!',
-      restaurants
+      title: 'You Betta\' Eat Rite!',
+      restaurants: restaurants.slice(randNum, randNum + 3)
     });
   });
 });
