12f3580441f3830963b8c1a348e21be9b912ca53
johnmberger <johnmartinberger@gmail.com>
2016-09-15 14:28:00 -0600

    fixed css

--- src/client/js/signinhelpers.js
+++ src/client/js/signinhelpers.js
@@ -3,7 +3,7 @@ function passwordValidation (req, res, next) {
   if (req.body.password !== req.body.confirm_password) {
     console.log('no match');
     const renderObject = {};
-    renderObject.alertMessage = "Your passwords do not match.";
+    renderObject.alertMessage = 'Your passwords do not match.';
     res.render('newUser', renderObject);
   } else {
     console.log('match');
