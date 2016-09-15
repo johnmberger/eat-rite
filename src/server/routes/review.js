a293ee7f9586334d70f21a6db7a85032fb404952
Allen Fordham <allen.s.fordham@gmail.com>
2016-09-15 11:39:11 -0600

    still tweaking review - linked to it on each restaurant page

--- src/server/routes/review.js
+++ src/server/routes/review.js
@@ -3,18 +3,16 @@ const router = express.Router();
 
 const indexController = require('../controllers/index');
 
-router.get('/', function (req, res, next) {
-  const renderObject = {};
-  renderObject.title = 'Review Page!';
-  indexController.sum(1, 2, (error, results) => {
-    if (error) return next(error);
-    if (results) {
-      renderObject.sum = results;
-      res.render('review', renderObject);
-    }
-  });
+router.get('/', (req, res, next) => {
+  const renderObj = {};
+  renderObj.title = 'Review Page';
+  res.render('review', renderObj);
 });
 
-
+router.get('/:id', (req, res, next) => {
+  const renderObj = {};
+  renderObj.title = 'Review Page';
+  res.render('review', renderObj);
+});
 
 module.exports = router;
