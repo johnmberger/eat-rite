897a6d3ccbb0b48d81d9b97521b31d624d285d98
Allen Fordham <allen.s.fordham@gmail.com>
2016-09-15 14:00:31 -0600

    adding ratings now works

--- src/server/routes/review.js
+++ src/server/routes/review.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db  = require('../db/knex');
+const knex  = require('../db/knex');
 
 router.get('/', (req, res, next) => {
   const renderObj = {};
@@ -10,19 +10,18 @@ router.get('/', (req, res, next) => {
 
 router.get('/:id', (req, res, next) => {
   let id = req.params.id;
-  db('restaurants').where('id', id)
+  knex('restaurants').where('id', id)
   .then((result) => {
     const renderObj = {};
     renderObj.title = 'Review Page';
     renderObj.result = result;
     res.render('review', renderObj);
-    console.log(renderObj);
   });
 });
 
 function oneRest(req, res, next) {
   let id = req.params.id;
-  db('restaurants').where('id', id)
+  knex('restaurants').where('id', id)
   .then((result) => {
     const renderObject = {};
     renderObject.rests = result;
@@ -34,4 +33,19 @@ function oneRest(req, res, next) {
   });
 }
 
+router.post('/:id', (req, res, next) => {
+  let id = req.params.id;
+  let newReview = {
+    restaurant_id: id,
+    created_at: req.body.reviewDate,
+    content: req.body.reviewText,
+    rating: req.body.rating
+  };
+  // console.log(newReview);
+  knex('reviews')
+  .insert(newReview)
+  .then (() => {
+    res.redirect(`/restaurant/${id}`);
+  });
+});
 module.exports = router;
