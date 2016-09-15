c24c1465d71ebda8670e8f5b0fcdfc76e1aed8e6
johnmberger <johnmartinberger@gmail.com>
2016-09-15 16:05:10 -0600

    added reviews to restaurant page

--- src/server/controllers/restaurant.js
+++ src/server/controllers/restaurant.js
@@ -1,18 +1,23 @@
-const knex  = require('../db/knex');
+const knex = require('../db/knex');
 
 function oneRest(searchID) {
   return Promise.all([
     knex('restaurants').where('id', searchID),
-    knex('reviews').where('restaurant_id', searchID)
+    knex('reviews').select('*', 'reviews.created_at as review_date').where('restaurant_id', searchID)
+    .leftJoin('users', 'users.id', 'reviews.user_id')
   ])
   .then((result) => {
-    const reviews = result[1];
+    var reviews = result[1];
+    console.log(reviews);
     var total = 0;
     reviews.forEach(review => {
       total += Number(review.rating);
+      console.log(review);
     });
     var average = total / reviews.length;
-    const renderObject = {};
+    var renderObject = {};
+    renderObject.numberReviews = reviews.length;
+    renderObject.allReviews = reviews;
     renderObject.title = result[0][0].name;
     renderObject.rests = result[0];
     renderObject.score = average;
