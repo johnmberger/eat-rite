f22cfbc7ad79a4023ac7dc79e8ab8214ba67671f
johnmberger <johnmartinberger@gmail.com>
2016-09-15 16:56:37 -0600

    added employees to restaurant page

--- src/server/controllers/restaurant.js
+++ src/server/controllers/restaurant.js
@@ -2,25 +2,28 @@ const knex = require('../db/knex');
 
 function oneRest(searchID) {
   return Promise.all([
-    knex('restaurants').where('id', searchID),
+    knex('restaurants').where('restaurants.id', searchID)
+    .leftJoin('employees', 'employees.restaurant_id', 'restaurants.id')
+    .orderBy('employees.role', 'DESC'),
     knex('reviews').select('*', 'reviews.created_at as review_date').where('restaurant_id', searchID)
     .leftJoin('users', 'users.id', 'reviews.user_id')
   ])
   .then((result) => {
+    console.log(result);
     var reviews = result[1];
-    console.log(reviews);
     var total = 0;
     reviews.forEach(review => {
       total += Number(review.rating);
-      console.log(review);
     });
     var average = total / reviews.length;
     var renderObject = {};
     renderObject.numberReviews = reviews.length;
     renderObject.allReviews = reviews;
     renderObject.title = result[0][0].name;
-    renderObject.rests = result[0];
+    renderObject.rest = result[0][0];
     renderObject.score = average;
+    renderObject.employees = result[0];
+    renderObject.id = searchID;
     return renderObject;
   }).catch((err) => {
     console.log(err);
