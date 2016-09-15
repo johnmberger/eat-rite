2f31c645d4c587a4c24a8953ca5ce1455705985f
Allen Fordham <allen.s.fordham@gmail.com>
2016-09-15 12:12:14 -0600

    reviews page links off each restaurant, and pulls restaurant data to populate the form

--- src/server/routes/review.js
+++ src/server/routes/review.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const db  = require('../db/knex');
+const Rests = db('restaurants');
 
 const indexController = require('../controllers/index');
 
@@ -10,9 +12,29 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/:id', (req, res, next) => {
-  const renderObj = {};
-  renderObj.title = 'Review Page';
-  res.render('review', renderObj);
+  let id = req.params.id;
+  Rests.where('id', id)
+  .then((result) => {
+    const renderObj = {};
+    renderObj.title = 'Review Page';
+    renderObj.result = result;
+    res.render('review', renderObj);
+    console.log(renderObj);
+  });
 });
+
+function oneRest(req, res, next) {
+  let id = req.params.id;
+  Rests.where('id', id)
+  .then((result) => {
+    const renderObject = {};
+    renderObject.rests = result;
+    if (result) {
+      res.status(200).render('restaurants/one-restaurant', renderObject);
+    } else {
+      res.status(404).send({message: 'Restaurant not found.'});
+    }
+  });
+}
 
 module.exports = router;
