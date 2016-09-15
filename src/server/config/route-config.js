114a5ebbc4380675717df786160ccb18a141c19e
Gina <gdebell@gmail.com>
2016-09-15 09:16:02 -0600

    first group merge

--- src/server/config/route-config.js
+++ src/server/config/route-config.js
@@ -6,10 +6,13 @@
 
     // *** routes *** //
     const routes = require('../routes/index');
+    const newUser = require('../routes/newuser');
+    const signIn = require('../routes/signin');
 
     // *** register routes *** //
     app.use('/', routes);
-
+    app.use('/newuser', newUser);
+    app.use('/signin', signIn);
   };
 
 })(module.exports);
