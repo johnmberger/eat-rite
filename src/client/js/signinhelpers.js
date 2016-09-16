2cee2fd85530c3bd41da091c704ac3d069a802c0
Gina <gdebell@gmail.com>
2016-09-16 11:48:38 -0600

    function: createUser and hashSalt to capture hashed password in database.

--- src/client/js/signinhelpers.js
+++ src/client/js/signinhelpers.js
@@ -2,40 +2,59 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../../server/db/knex');
 
+const md5 = require('md5');
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
+const hash = bcrypt.hashSync('B4c0/\/', salt);
+
 function passwordValidation (req, res, next) {
   const userEmail = req.body.email;
-  const renderObject = {};
-
   knex('users')
-  .then (users => {
-      var getOne = users.map(user => {
-        //console.log(user.email);    //all email in db
-        return user.email;
-      });
-    })
-    .then (getOne => {
-    console.log(getOne);
-    if (req.body.password !== req.body.confirm_password) {
+  .where('email', userEmail)
+  .then ((user) => {
+    const renderObject = {};
+    if (user.length) {
+      renderObject.alertIsUser = 'You already have an account set up with that email.';
+      res.render('newUser', renderObject);
+    } else if (req.body.password !==  req.body.confirm_password) {
       renderObject.alertMessage = 'Your passwords do not match.';
       res.render('newUser', renderObject);
-    } else if (userEmail === userEmail) {
-      renderObject.alertIsUser = 'You already have an account with us.';
+    } else if (userEmail === undefined) {
+      renderObject.alertNoEmail = 'You did not type in an email.';
+      res.render('newUser', renderObject);
     } else {
       console.log('match');
-      next();
     }
+    next();
   });
+}
+//hashing salt
+function hashSalt (password) {
+  var hashed = bcrypt.hashSync(password, 2);
+  console.log('hashed one', hashed);
+  return (hashed);
+}
+//create a user inside the datebase
+function createUser(req, userObject) {
+  console.log('in the createUser FUNCTION req', req);
+  console.log('in the createUser FUNCTION for email', req.password);
+  var pass = hashSalt(req.password);
+  console.log('PASSSSSSSS', pass);
+  console.log('PASS VARIABLE', `${pass}`);
+
+  var newUser = {
+    first_name: req.first_name,
+    last_name: req.last_name,
+    email: req.email,
+    password: `${pass}`,
+    is_admin: req.is_admin || false
+  };
+  console.log('newusers.......', newUser);
+  return knex('users').insert(newUser);
 }
 
 module.exports = {
-  passwordValidation
+  passwordValidation,
+  hashSalt,
+  createUser
 };
-
-// Log all cocktails.
-//expected output ['Old Fashined', 'Pisco Sour', 'Manhattan']
-// knex('cocktails')
-// .then(cocktails => {
-//   return cocktails.map(function (cocktail) {
-//     return cocktail.name
-//   })
-// })
