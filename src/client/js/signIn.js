3344fafeb1636971635ca9175cc12a182b2092b1
DickinsonMark <monoposon@comcast.net>
2016-09-16 15:49:14 -0600

    finished the login page

--- src/client/js/signIn.js
+++ src/client/js/signIn.js
@@ -11,10 +11,10 @@ $('#signIn').on('click', (e) => {
     method: 'POST',
     data: payload
   }).done((data) => {
-    if(data.message) {
+    if (data.message) {
       window.location.replace('/');
     } else {
-      console.log(data);
+      $('#error').html(`<div>${data.error}</div>`);
     }
   });
 });
