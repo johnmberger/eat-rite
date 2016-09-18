4caf9fc30f2c5ee0cddfaa636f625a7e9c991e83
Shane H Pittman <shanehpittman@gmail.com>
2016-09-18 12:14:39 -0600

    update reviews

--- src/server/routes/review.js
+++ src/server/routes/review.js
@@ -1,22 +1,52 @@
 const express = require('express');
 const router = express.Router();
 const knex  = require('../db/knex');
+const one_Rest = require('../controllers/restaurant').oneRest;
 
 router.get('/', (req, res, next) => {
   const renderObj = {};
   renderObj.title = 'Review Page';
   res.render('review', renderObj);
 });
-router.get('/:id', (req, res, next) => {
-  let id = req.params.id;
-  knex('restaurants').where('id', id)
-  .then((result) => {
+router.get('/:id/edit-review/:id2', (req, res, next) => {
+  let searchID = req.params.id;
+  let restId =  req.params.id2;
+  Promise.all([
+    knex('reviews')
+    .select('*', 'reviews.created_at as review_date')
+    .where('restaurant_id', restId)
+    .andWhere('user_id', searchID).first()
+  ]).then((result) => {
     const renderObj = {};
-    renderObj.title = 'Review Page';
+    renderObj.title = 'Edit Review';
     renderObj.result = result;
-    res.render('review', renderObj);
+    res.render('edit-review', renderObj);
   });
 });
+
+router.put('/edit-review', reviewUpdate);
+function reviewUpdate(req, res, next) {
+  let ids = req.headers.referer.split('/');
+  let rev = ids[4];
+  let rest = ids[6];
+  const update = {
+    content: req.body.content,
+    rating: parseInt(req.body.rating)
+  };
+  knex('reviews')
+  .select('*', 'reviews.created_at as review_date')
+  .where('restaurant_id', rest)
+  .andWhere('user_id', rev)
+  .update({
+    content: req.body.content,
+    rating: parseInt(req.body.rating)
+  })
+  .then((results) => {
+    res.status(200).json({
+      status: 'success'
+    });
+  });
+}
 
 function oneRest(req, res, next) {
   let id = req.params.id;
