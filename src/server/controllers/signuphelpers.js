79430e203e685856ceb0f0aac237f57c31e9e7f5
johnmberger <johnmartinberger@gmail.com>
2016-09-18 22:20:45 -0600

    messing with renderObject error

--- src/server/controllers/signuphelpers.js
+++ src/server/controllers/signuphelpers.js
@@ -6,40 +6,34 @@ function passwordValidation (req, res, next) {
   knex('users')
   .where('email', userEmail)
   .then ((user) => {
+    var renderObject = {};
     var str = req.body.password;
     var upper = (/[A-Z]/.test(str));
     var lower = (/[a-z]/.test(str));
     var number = (/[0-9]/.test(str));
 
     if (user.length) {
-      const renderObject = {};
       renderObject.alertIsUser = 'You already have an account set up with that email.';
       req.body.err = true;
       res.render('newUser', renderObject);
     } else if (req.body.first_name.length === 0 || req.body.last_name.length === 0 || req.body.email.length === 0 || req.body.password.length === 0 || req.body.confirm_password.length === 0) {
-      const renderObject = {};
       renderObject.alertShort = 'You have at least one empty field.';
       res.render('newUser', renderObject);
-    }else if (req.body.password.length < 8) {
-      const renderObject = {};
+    } else if (req.body.password.length < 8) {
       renderObject.alertPass = 'Your password needs to be at least 8 characters long.';
       res.render('newUser', renderObject);
     } else if (req.body.first_name.length > 41 || req.body.last_name.length > 41) {
-      const renderObject = {};
       renderObject.alertLength = 'Your name is too long. Please use less than 40 characters.';
       res.render('newUser', renderObject);
     } else if (req.body.password !==  req.body.confirm_password) {
-      const renderObject = {};
       renderObject.alertMessage = 'Your passwords do not match.';
       req.body.err = true;
       res.render('newUser', renderObject);
     } else if (userEmail === undefined) {
-      const renderObject = {};
       renderObject.alertNoEmail = 'You did not type in an email.';
       req.body.err = true;
       res.render('newUser', renderObject);
     } else if (upper === false || lower === false || number === false) {
-      const renderObject = {};
       renderObject.valPass = 'Your password does not meet our requirements.';
       req.body.err = true;
       res.render('newUser', renderObject);
