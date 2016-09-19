6f60e231a81c64e8991dde7fec8dbba1259e2e91
johnmberger <johnmartinberger@gmail.com>
2016-09-18 21:47:25 -0600

    trying to wire up db

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,10 @@
-const databaseName = 'gRestaurants';
+const databaseName = 'dapdc7h58im0eg';
 
 module.exports = {
   development: {
     client: 'postgresql',
-    connection: `postgres://localhost:5432/${databaseName}`,
+    connection: `postgres://jndwsaairtjett:vilTy6SnyMLVC1kkRkpAbtkc5j@ec2-54-225-81-90.compute-1.amazonaws.com:5432/dapdc7h58im0eg`,
+    port: 5432
     migrations: {
       directory: __dirname + '/src/server/db/migrations'
     },
