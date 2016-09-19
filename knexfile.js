1a5de61011aa65c34a05fa039492df22759586e8
johnmberger <johnmartinberger@gmail.com>
2016-09-18 21:48:43 -0600

    fixing typos

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: `postgres://jndwsaairtjett:vilTy6SnyMLVC1kkRkpAbtkc5j@ec2-54-225-81-90.compute-1.amazonaws.com:5432/dapdc7h58im0eg`,
-    port: 5432
+    port: 5432,
     migrations: {
       directory: __dirname + '/src/server/db/migrations'
     },
